db494c107cb2b2cc6831e14bdbd2d38662a4048d
Italo Correia <italocdaraujo@gmail.com>
2025-05-21 18:36:12 -0300

    change:  cors permission

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Ou '*' para permitir qualquer origem
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
